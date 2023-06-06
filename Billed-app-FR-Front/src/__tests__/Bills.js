7b748a2a3f35f1d80e8959bc65da9f3d84b45cf4
SW49GO <gohard.so49@gmail.com>
2023-06-06 13:42:20 +0200

    Bills & NewBills units tests

--- Billed-app-FR-Front/src/__tests__/Bills.js
+++ Billed-app-FR-Front/src/__tests__/Bills.js
@@ -2,12 +2,15 @@
  * @jest-environment jsdom
  */
 
-import {screen, waitFor} from "@testing-library/dom"
+import {screen, waitFor, fireEvent} from "@testing-library/dom"
+import userEvent from '@testing-library/user-event'
 import BillsUI from "../views/BillsUI.js"
 import { bills } from "../fixtures/bills.js"
-import { ROUTES_PATH} from "../constants/routes.js";
+import { ROUTES_PATH, ROUTES} from "../constants/routes.js";
+import Logout from "../containers/Logout";
 import {localStorageMock} from "../__mocks__/localStorage.js";
-
+import mockStore from "../__mocks__/store"
+jest.mock("../app/store", () => mockStore)
 import router from "../app/Router.js";
 
 describe("Given I am connected as an employee", () => {
@@ -28,12 +31,130 @@ describe("Given I am connected as an employee", () => {
       //to-do write expect expression
       expect(windowIcon.classList.contains("active-icon")).toBe(true);
     })
+
     test("Then bills should be ordered from earliest to latest", () => {
       document.body.innerHTML = BillsUI({ data: bills })
       const dates = screen.getAllByText(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i).map(a => a.innerHTML)
       const antiChrono = (a, b) => ((a < b) ? 1 : -1)
       const datesSorted = [...dates].sort(antiChrono)
       expect(dates).toEqual(datesSorted)
+    })
+  })
+
+  describe("When I click on the 'New Bill' button",()=>{
+    test("Then I am directed to the NewBill page",()=>{
+      Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+      window.localStorage.setItem('user', JSON.stringify({
+        type: 'Employee'
+      }))
+      const root = document.createElement("div");
+      root.setAttribute("id", "root");
+      document.body.append(root);
+      router()
+      window.onNavigate(ROUTES_PATH.Bills);
+
+      const handleClickNewBill = jest.fn();
+      const buttonNewBill = screen.getByTestId('btn-new-bill');
+      buttonNewBill.addEventListener('click', handleClickNewBill);
+      userEvent.click(buttonNewBill);
+      expect(handleClickNewBill).toHaveBeenCalled();
+      expect(screen.getByText("Envoyer une note de frais")).toBeTruthy()
+    })
+  })
+})
+
+describe("Given I am connected as an employee, I am on Bill page", () => {
+  describe("When I click on the 'Back' button of the browser",()=>{
+    test("Then I stay on Bills page",()=>{
+      Object.defineProperty(window, 'localStorage', { value: localStorageMock})
+      window.localStorage.setItem('user', JSON.stringify({type: 'Employee'}))
+      const root = document.createElement("div")
+      root.setAttribute("id", "root")
+      document.body.append(root)
+      router()
+      window.onNavigate(ROUTES_PATH.Bills)
+
+      const PREVIOUS_LOCATION ='/';
+      
+            window.onpopstate = (e) => {
+              const user = JSON.parse(localStorage.getItem('user'))
+              if (window.location.pathname === "/" && !user) {
+                document.body.style.backgroundColor="#0E5AE5"
+                root.innerHTML = ROUTES({ pathname: window.location.pathname })
+              }
+              else if (user) {
+                onNavigate(PREVIOUS_LOCATION)
+              }
+            }
+        window.onpopstate();
+        expect(document.body.innerHTML).toContain('Mes notes de frais');
+    })
+  })
+
+  describe("When I click on the iconEye of one of the expense reports",()=>{
+    test("Then A modal opens displaying the Proof of the expense report",()=>{
+      Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+      window.localStorage.setItem('user', JSON.stringify({
+        type: 'Employee'
+      }))
+      const root = document.createElement("div");
+      root.setAttribute("id", "root");
+      document.body.append(root);
+      router()
+      window.onNavigate(ROUTES_PATH.Bills);
+      $.fn.modal = jest.fn();
+      const handleClickIconEye = jest.fn();
+      const iconEyes = screen.getAllByTestId("icon-eye");
+      if (iconEyes.length > 0) {
+        const firstIconEyes = iconEyes[0]
+        firstIconEyes.addEventListener("click", () => handleClickIconEye(firstIconEyes));
+        userEvent.click(firstIconEyes);
+        expect(handleClickIconEye).toHaveBeenCalled();
+        expect($.fn.modal).toHaveBeenCalled();
+      }
+      expect(screen.getByText("Justificatif")).toBeTruthy();
+    })
+  })
+  // describe("When I click on disconnect Button",()=>{
+  //   test("Then I am redirected to the 'Login' page",()=>{
+  //     const onNavigate = (pathname) => {
+  //       document.body.innerHTML = ROUTES({ pathname })
+  //     }
+  //     Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+  //     window.localStorage.setItem('user', JSON.stringify({
+  //       type: 'Employee'
+  //     }))
+  //     document.body.innerHTML = BillsUI({ data:bills })
+  //     const userData = window.localStorage.getItem('user');
+  //     expect(userData).toBeDefined();
+  //     console.log(JSON.parse(userData));
+  //     const logout = new Logout({ document, onNavigate, localStorage })
+  //     const disconnect = document.getElementById('layout-disconnect')
+  //     const handleClick = jest.fn(logout.handleClick)
+  //     disconnect.addEventListener('click', handleClick)
+  //     userEvent.click(disconnect)
+  //     expect(handleClick).toHaveBeenCalled()
+  //     expect(screen.getByText('Employé')).toBeTruthy()
+  //   })
+  // })
+})
+
+describe("Given I am connected as an employee, on Bills Page, and I click on EyeIcon",()=>{
+  describe("When I click on the closing 'Cross'",()=>{
+    test("Then the modal disappear",()=>{
+      document.body.innerHTML=BillsUI({data:bills});
+      $.fn.modal = jest.fn();
+      const handleClickIconEye = jest.fn();
+      const firstIconEyes = screen.getAllByTestId("icon-eye")[0];
+      firstIconEyes.addEventListener("click", () => handleClickIconEye(firstIconEyes));
+      fireEvent.click(firstIconEyes);
+
+      const modalClose = document.body.querySelector('.close')
+      const clickHandlerClose = jest.fn();
+      modalClose.addEventListener('click', clickHandlerClose);
+      userEvent.click(modalClose);
+      const modalContainer = document.body.querySelector('.modal')
+      expect(modalContainer.classList.contains('show')).toBeFalsy();
     })
   })
 })
--- Billed-app-FR-Front/src/__tests__/NewBill.js
+++ Billed-app-FR-Front/src/__tests__/NewBill.js
@@ -2,17 +2,131 @@
  * @jest-environment jsdom
  */
 
-import { screen } from "@testing-library/dom"
+import { screen,fireEvent } from "@testing-library/dom"
+import userEvent from '@testing-library/user-event'
 import NewBillUI from "../views/NewBillUI.js"
 import NewBill from "../containers/NewBill.js"
+import {localStorageMock} from "../__mocks__/localStorage.js";
+import mockStore from "../__mocks__/store.js"
+import { ROUTES, ROUTES_PATH } from '../constants/routes.js'
+import router from "../app/Router.js";
+jest.mock("../app/store", () => mockStore)
 
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on NewBill Page", () => {
-    test("Then ...", () => {
+    test("Then I visualize the form 'Send a Bills'", () => {
       const html = NewBillUI()
       document.body.innerHTML = html
-      //to-do write assertion
+      const form = screen.getByText("Envoyer une note de frais")
+      expect(form).toBeTruthy()
+    })
+  })
+})
+describe("Given I am connected as an employee, I am on NewBill Page",()=>{
+  describe("When I click on the 'Back' button of the browser",()=>{
+    test("Then I am redirected to the Bills page",()=>{
+      Object.defineProperty(window, 'localStorage', { value: localStorageMock})
+      window.localStorage.setItem('user', JSON.stringify({type: 'Employee'}))
+      const root = document.createElement("div")
+      root.setAttribute("id", "root")
+      document.body.append(root)
+      router()
+      window.onNavigate(ROUTES_PATH.NewBill)
+      const onNavigate = (pathname) => {
+        document.body.innerHTML = ROUTES({ pathname })
+      }
+      const PREVIOUS_LOCATION ='#employee/bills';
+  
+      window.onpopstate = (e) => {
+        const user = JSON.parse(localStorage.getItem('user'))
+        if (window.location.pathname === "/" && !user) {
+          document.body.style.backgroundColor="#0E5AE5"
+          root.innerHTML = ROUTES({ pathname: window.location.pathname })
+        }
+        else if (user) {
+          onNavigate(PREVIOUS_LOCATION)
+        }
+      }
+      window.onpopstate();
+      expect(document.body.innerHTML).toContain('Mes notes de frais');
+    })
+  })
+
+  describe("When I do not fill in the fields of the required form, I click on the 'Send' button",()=>{
+    test("Then I am told to fill in the required fields, I still stay on form",()=>{
+      document.body.innerHTML=NewBillUI();
+      const form = screen.getByTestId("form-new-bill")
+      const buttonNewBill = jest.fn((e) => e.preventDefault())
+      form.addEventListener("submit", buttonNewBill)
+      fireEvent.submit(form)
+
+      expect(form).toBeTruthy()
+      expect(document.getElementById("btn-send-bill")).toBeTruthy()
+    })
+  })
+
+  describe("When I fill out the form with the wrong file format",()=>{
+    test("Then I can't click the 'Submit' button",()=>{
+      document.body.innerHTML =NewBillUI() 
+      const onNavigate = (pathname) => {
+        document.body.innerHTML = ROUTES({ pathname })
+      }
+
+      const newBill = new NewBill({ document, onNavigate, store:null, localStorage: window.localStorage })
+
+      const downloadFile = jest.fn((e) => newBill.handleChangeFile(e))
+      
+      const file = screen.getByTestId("file")
+      const myTestFile = new File(["blablabla"], "monfichier.txt", { type: "text/plain" });
+      file.addEventListener("change",downloadFile)
+      fireEvent.change(file, {target: {files: [myTestFile]}})
+
+      expect(downloadFile).toHaveBeenCalled()
+      const consoleSpy = jest.spyOn(console, 'log');
+      expect(consoleSpy).toBeTruthy()
+      const buttonSubmit = screen.getByText("Envoyer")
+      expect(buttonSubmit.disabled).toBe(true)
+    })
+  })
+
+  describe("When I fill in the form with a good file format (jpg,jpeg,png,gif), I can click on the 'Send' button",()=>{
+    test("Then I am redirected to the Bills page with display of the new 'Bill' with a 'pending' status", ()=>{
+      document.body.innerHTML =NewBillUI() 
+      const onNavigate = (pathname) => {
+        document.body.innerHTML = ROUTES({ pathname })
+      }
+      const newBill = new NewBill({ document, onNavigate, store:mockStore, localStorage: window.localStorage })
+      const formNewBill = screen.getByTestId("form-new-bill")
+
+      userEvent.selectOptions(formNewBill.querySelector(`select[data-testid="expense-type"]`), 'Transports');
+      userEvent.type(formNewBill.querySelector(`input[data-testid="expense-name"]`), 'Apero collègue');
+      userEvent.type(formNewBill.querySelector(`input[data-testid="amount"]`), '100');
+      userEvent.type(formNewBill.querySelector(`input[data-testid="vat"]`), '20');
+      userEvent.type(formNewBill.querySelector(`input[data-testid="pct"]`), '10');
+      userEvent.type(formNewBill.querySelector(`textarea[data-testid="commentary"]`), 'C\'était cool !!');
+      const dateInput = formNewBill.querySelector(`input[data-testid="datepicker"]`);
+      dateInput.setAttribute('value', '2020-05-01');
+      userEvent.click(dateInput); // 
+  
+      newBill.fileUrl = 'https://example.com/file-url';
+      newBill.fileName = 'example-file.jpg';
+
+      const updateBillSpy = jest.spyOn(newBill, "updateBill");
+      const handleSubmit = jest.spyOn(newBill,"handleSubmit");
+
+      handleSubmit({
+        preventDefault: jest.fn(),
+        target: formNewBill,
+      });
+
+      expect(updateBillSpy).toHaveBeenCalled();
+      expect(screen.getByText("Mes notes de frais")).toBeTruthy();
+
+      const updateBillArgs = updateBillSpy.mock.calls[0];
+      const [billData] = updateBillArgs;
+      expect(billData.name).toBe('Apero collègue');
+      expect(billData.status).toBe('pending');
     })
   })
 })
